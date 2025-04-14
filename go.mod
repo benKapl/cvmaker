4ccc1b593e08e06e89557a24046cf02e21f1a01b
benKapl <benji1.kaplan@gmail.com>
2025-04-14 17:44:40 +0200

    added api first configuration with database parameters. Error on launching server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cvmaker_api
 
 go 1.23.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"cvmaker_api/internal/database"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type apiConfig struct {
+	db        *database.Queries
+	platform  string
+	JWTsecret string
+	port      string
+}
+
 func main() {
-	const port = "8080"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+	JWTSecret := os.Getenv("JWT_SECRET")
+	if JWTSecret == "" {
+		log.Fatal("JWTSecret must be set")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("port must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error connecting to database")
+	}
+	defer dbConn.Close()
+	dbQueries := database.New(dbConn)
+
+	apiCfg := apiConfig{
+		db:        dbQueries,
+		platform:  platform,
+		JWTsecret: JWTSecret,
+		port:      port,
+	}
 
 	mux := http.NewServeMux()
 
 	srv := &http.Server{
-		Addr:    ":" + port,
+		Addr:    ":" + apiCfg.port,
 		Handler: mux,
 	}
 
