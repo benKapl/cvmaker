29c67ff24ec7a46caca2276c874f047d99c8910c
benKapl <benji1.kaplan@gmail.com>
2025-04-23 16:07:52 +0200

    created Offer struct

--- handler_offers_create.go
+++ handler_offers_create.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Offer struct {
+	ID                      uuid.UUID `json:"id"`
+	CreatedAt               time.Time `json:"created_at"`
+	UpdatedAt               time.Time `json:"updated_at"`
+	Label                   string    `json:"label"`
+	Organization            string    `json:"organization"`
+	OrganizationDescription *string   `json:"organization_description"`
+	Missions                string    `json:"missions"`
+	Stack                   *string   `json:"stack"`
+	ExpectedProfile         string    `json:"expected_profile"`
+	Miscellaneous           *string   `json:"miscellaneous"`
+	UserID                  uuid.UUID `json:"user_id"`
+}
+
+func handlerOffersCreate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		body string
+	}
+
+	type response struct {
+		Offer
+	}
+}
