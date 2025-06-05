3277f4b125166e5fa32230dc1edf8db551ca3fde
benKapl <benji1.kaplan@gmail.com>
2025-06-05 22:43:48 +0200

    docs(api): cleaned main.go file and commented it

--- api/main.go
+++ api/main.go
@@ -16,42 +16,34 @@ func main() {
 	// Load Configuration
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal("Error loading configuration: %v", err)
+		log.Fatalf("Error loading configuration: %v", err)
 	}
 
 	// Initialize Dependencies
 	dbConn, err := sql.Open("postgres", cfg.DBURL)
 	if err != nil {
-		log.Fatal("Error connecting to database: %v", err)
+		log.Fatalf("Error connecting to database: %v", err)
 	}
 	defer dbConn.Close() // Ensure connection is closed on exit
 
 	if err := dbConn.Ping(); err != nil {
-		log.Fatal("Error pinging database: %v", err)
+		log.Fatalf("Error pinging database: %v", err)
 	} else {
 		log.Println("Database connection verified")
 	}
 
 	dbQueries := database.New(dbConn)
-
-	llmClient := llm.NewClient(cfg.LLMTimeout) // significant timeout to handle llm response time
+	llmClient := llm.NewClient(cfg.LLMTimeout)
 
 	api := handlers.NewAPI(dbQueries, llmClient, cfg.JWTSecret, cfg.Platform)
 
-	// prompt := llm.OfferPromptStart + "I am the company Decathlon, i am recruiting a salesman. The salary is 10.000 euros per year. You misson will be to sell our products" + llm.OfferPromptEnd
-	// go func() {
-	// 	res, err := apiCfg.llmClient.Generate(prompt, llm.OfferFormat)
-	// 	if err != nil {
-	// 		log.Fatalf("Error: %s", err)
-	// 	}
-	// 	fmt.Println(res)
-	// }()
-
+	// Setup router and routes
 	mux := http.NewServeMux()
 	api.RegisterRoutes(mux)
 
 	globalMux := handlers.LoggingMiddleware(mux)
 
+	// Start Server
 	srv := &http.Server{
 		Addr:    ":" + cfg.Port,
 		Handler: globalMux,
