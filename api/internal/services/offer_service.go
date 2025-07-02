6d7898f191014d843d7486960565bdbefcefb697
benKapl <benji1.kaplan@gmail.com>
2025-07-02 16:34:00 +0200

    refactor(api): created new CreateOffer method in services package

---
+++ api/internal/services/offer_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/benKapl/cvmaker-api/internal/database"
+	"github.com/benKapl/cvmaker-api/internal/llm"
+	"github.com/benKapl/cvmaker-api/internal/prompter"
+	"github.com/google/uuid"
+)
+
+type OfferService struct {
+	DB        *database.Queries
+	LLMClient llm.LLMClient
+}
+
+func (s *OfferService) CreateOffer(ctx context.Context, userID uuid.UUID, rawOffer string) (database.Offer, error) {
+	parsedOffer, err := prompter.ParseOffer(ctx, rawOffer, s.LLMClient)
+	if err != nil {
+		return database.Offer{}, fmt.Errorf("couldn't parse offer: %w", err)
+	}
+
+	dbParams := database.CreateOfferParams{
+		Title:           parsedOffer.Title,
+		Organization:    parsedOffer.Organization,
+		Missions:        parsedOffer.Missions,
+		Stack:           parsedOffer.Stack,
+		ExpectedProfile: parsedOffer.ExpectedProfile,
+		Miscellaneous:   parsedOffer.Miscellaneous,
+		UserID:          userID,
+	}
+	// Map optional string field (OrganizationDescription) from *string to sql.NullString
+	if parsedOffer.OrganizationDescription != nil {
+		dbParams.OrganizationDescription = sql.NullString{String: *parsedOffer.OrganizationDescription, Valid: true}
+	} else {
+		dbParams.OrganizationDescription = sql.NullString{Valid: false}
+	}
+
+	// Create offer in database
+	offer, err := s.DB.CreateOffer(ctx, dbParams)
+	if err != nil {
+		return database.Offer{}, fmt.Errorf("couldn't create offer in db: %w", err)
+	}
+
+	return offer, nil
+}
