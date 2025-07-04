7394f6aede3ddabe6f58569c95fe9f61cc102ed4
benKapl <benji1.kaplan@gmail.com>
2025-07-04 11:34:47 +0200

    refactor(api): removed db dependency to api struct, now container only services structs

--- api/internal/handlers/api.go
+++ api/internal/handlers/api.go
@@ -1,21 +1,18 @@
 package handlers
 
 import (
-	"github.com/benKapl/cvmaker-api/internal/database"
 	"github.com/benKapl/cvmaker-api/internal/services"
 )
 
 type API struct {
-	DB             *database.Queries
 	AdminService   *services.AdminService
 	AuthService    *services.AuthService
 	OfferService   *services.OfferService
 	ProfileService *services.ProfileService
 }
 
-func NewAPI(db *database.Queries, adminSrv *services.AdminService, authSrv *services.AuthService, offerSrv *services.OfferService, profileSrv *services.ProfileService) *API {
+func NewAPI(adminSrv *services.AdminService, authSrv *services.AuthService, offerSrv *services.OfferService, profileSrv *services.ProfileService) *API {
 	return &API{
-		DB:             db,
 		AdminService:   adminSrv,
 		AuthService:    authSrv,
 		OfferService:   offerSrv,
